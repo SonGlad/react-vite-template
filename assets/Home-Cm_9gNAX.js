ab65679a74ac732caa61b969b8cbecb70f6b9cb3
SonGlad <121556627+SonGlad@users.noreply.github.com>
2025-04-22 17:40:14 +0000

    Deploying to gh-pages from @ SonGlad/react-vite-template@f8998747c8c72fe4ed1d2aa169ff6cef1b0ad509 🚀

---
+++ assets/Home-Cm_9gNAX.js
@@ -0,0 +1,27 @@
+import{d as t,j as i}from"./index-BPlA5mPc.js";const e=t.section`
+	/* min-height: 100vh;
+    display: flex;
+    align-items: center;
+	flex-direction: column;
+    font-size: 40px;
+	font-family: 'Open Sans', sans-serif;
+    color: #fff; */
+`,s=({children:n})=>i.jsx(e,{children:n}),a=t.div`
+  min-width: 300px;
+  margin: 0 auto;
+  /* max-width: 320px; */
+  padding: 0 10px;
+
+  /* outline: 2px solid red;
+  outline-offset: -2px; */
+
+  @media screen and (min-width: 768px) {
+    /* max-width: 834px; */
+    padding: 0 27px;
+  }
+
+  @media screen and (min-width: 1440px) {
+    /* max-width: 1440px; */
+    padding: 0 34px;
+  }
+`,o=({children:n})=>i.jsx(a,{children:n}),x=()=>i.jsx(s,{children:i.jsx(o,{children:i.jsx("h1",{children:"HELLO it`s a REACT + Vite template for multi-page application"})})});export{x as default};
